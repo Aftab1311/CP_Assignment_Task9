4bd198c925eee78f7f56c050616306d9687da718
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-25 19:48:09 +0530

    Update task9.cpp

--- task9.cpp
+++ task9.cpp
@@ -1,3 +1,12 @@
+/* Q.1 Check if the sum of a subarray within a given range is a perfect square or not 
+Given an array arr[] of size N and an array range[], the task is to check if the sum of the subarray {range[0],  .. , range[1]} is a perfect square or not. If the sum is a perfect square, then print the square root of the sum.  Otherwise, print -1. 
+Input: arr[] = {2, 19, 33, 48, 90, 100}, range = [1, 3] 
+Output: 10 
+Explanation: The sum of element from position 1 to position 3 is 19 + 33 + 48 = 100, which is a perfect  square of 10. 
+Input: arr[] = {13, 15, 30, 55, 87}, range = [0, 1] 
+Output: -1 
+*/
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
